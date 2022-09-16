d3a5fa15109f23b02727e712c4c27a17178ed276
muntako <akhmadmuntako@gmail.com>
2022-09-16 10:05:02 +0700

    second attempt

--- promise.js
+++ promise.js
@@ -6,31 +6,12 @@ const promiseOutput = async (emosi) => {
     // gunakan kata await untuk mendapatkan "data" hasil resolve-nya
     const ixx = await promiseTheaterIXX();
     const vgc = await promiseTheaterVGC();
-    let jumlahEmosi = 0;
-
-    return new Promise((resolve, reject) => {
-      // Kondisi gagal
-      if (emosi === null) {
-        reject('Maaf, emosi tidak boleh kosong!');
-      }
-      // Kondisi terpenuhi
-      else {
-        // iterate film ixx
-        for (film of ixx){
-          if (film.hasil == emosi){
-            jumlahEmosi++;
-          }
-        }
-        // iterate film vgc
-        for (film of vgc){
-          if (film.hasil == emosi){
-            jumlahEmosi++;
-          }
-        }
-        resolve(jumlahEmosi);
-      }    
+    let counter = 0;
+    return new Promise((resolve) => {      
+        counter += ixx.filter((item) => item.hasil === emosi).length;
+        counter += vgc.filter((item) => item.hasil === emosi).length;
+        resolve(counter);
     });
-    
   } catch (err) {
     console.log(err);
   }
